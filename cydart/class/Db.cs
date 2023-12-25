b99cc92c20dfb52bb9178b0dab0614d5f33e486c
Ceyda Tak <ceydatak03@gmail.com>
2023-12-25 22:40:15 +0300

    add database connection and Db class

---
+++ cydart/class/Db.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace cydart
+{
+    public class Db
+    {
+        public SqlConnection baglanti = new SqlConnection(ConfigurationManager.ConnectionStrings["dbyol"].ConnectionString);
+
+        public void ac()
+        {
+            baglanti.Open();
+        }
+
+        public void kapat()
+        {
+            baglanti.Close();
+        }
+    }
+}
