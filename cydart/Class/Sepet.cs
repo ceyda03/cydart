4bd7096a2491067f6400644f5342662ccfec3755
Ceyda Tak <ceydatak03@gmail.com>
2024-02-22 23:28:05 +0300

    added cart classes

---
+++ cydart/Class/Sepet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cydart.Class
+{
+    public class Sepet
+    {
+        int sipID, musID, detayID, urunID, tutarID, miktar;
+        double birimFiyat, topFiyat, kargoUcret, araToplam, genToplam;
+        DateTime sipTar;
+        byte satisDurum, sipDurum;
+
+        public int SipID { get => sipID; set => sipID = value; }
+        public int MusID { get => musID; set => musID = value; }
+        public int DetayID { get => detayID; set => detayID = value; }
+        public int UrunID { get => urunID; set => urunID = value; }
+        public int TutarID { get => tutarID; set => tutarID = value; }
+        public int Miktar { get => miktar; set => miktar = value; }
+        public double BirimFiyat { get => birimFiyat; set => birimFiyat = value; }
+        public double TopFiyat { get => topFiyat; set => topFiyat = value; }
+        public double KargoUcret { get => kargoUcret; set => kargoUcret = value; }
+        public double AraToplam { get => araToplam; set => araToplam = value; }
+        public double GenToplam { get => genToplam; set => genToplam = value; }
+        public DateTime SipTar { get => sipTar; set => sipTar = value; }
+        public byte SatisDurum { get => satisDurum; set => satisDurum = value; }
+        public byte SipDurum { get => sipDurum; set => sipDurum = value; }
+    }
+}
---
+++ cydart/Class/SepetCRUD.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Web;
+
+namespace cydart.Class
+{
+    public class SepetCRUD
+    {
+        Db db = new Db();
+
+        public bool sipekle(int musid, DateTime siptar)
+        {
+            bool cevap = true;
+            db.ac();
+            SqlCommand komut = new SqlCommand("insert into Siparis values(@musid, @tar, 0, 0)", db.baglanti);
+            komut.Parameters.AddWithValue("@musid", musid);
+            komut.Parameters.AddWithValue("@tar", siptar);
+            int sonuc = komut.ExecuteNonQuery();
+
+            if (sonuc == 0)
+            {
+                cevap = false;
+            }
+            db.kapat();
+            return cevap;
+        }
+
+        public bool detayekle(int sipid, int urunid, double bfiyat, int miktar, double tfiyat)
+        {
+            bool cevap = true;
+            db.ac();
+            SqlCommand komut = new SqlCommand("insert into S_Detay values(@sipid, @uid, @bfiyat, @miktar, @tfiyat)", db.baglanti);
+            komut.Parameters.AddWithValue("@sipid", sipid);
+            komut.Parameters.AddWithValue("@uid", urunid);
+            komut.Parameters.AddWithValue("@bfiyat", bfiyat);
+            komut.Parameters.AddWithValue("@miktar", miktar);
+            komut.Parameters.AddWithValue("@tfiyat", tfiyat);
+            int sonuc = komut.ExecuteNonQuery();
+
+            if (sonuc == 0)
+            {
+                cevap = false;
+            }
+            db.kapat();
+            return cevap;
+        }
+
+        public bool detayekle(int sipid, double aratop, double kargo, double geneltop)
+        {
+            bool cevap = true;
+            db.ac();
+            SqlCommand komut = new SqlCommand("insert into Tutar_Detay values(@sipid, @ara, @kargo, @genel)", db.baglanti);
+            komut.Parameters.AddWithValue("@sipid", sipid);
+            komut.Parameters.AddWithValue("@ara", aratop);
+            komut.Parameters.AddWithValue("@kargo", kargo);
+            komut.Parameters.AddWithValue("@genel", geneltop);
+            int sonuc = komut.ExecuteNonQuery();
+
+            if (sonuc == 0)
+            {
+                cevap = false;
+            }
+            db.kapat();
+            return cevap;
+        }
+        
+        public DataTable siplistele(int musid)
+        {
+            DataTable dt = new DataTable();
+            db.ac();
+            SqlCommand komut = new SqlCommand("select * from Siparis where Mus_id=@a and Satis_Durum=0", db.baglanti);
+            komut.Parameters.AddWithValue("@a", musid);
+            SqlDataAdapter adp = new SqlDataAdapter(komut);
+            adp.Fill(dt);
+            db.kapat();
+            return dt;
+        }
+
+        public DataTable sepetliste(int sipid)
+        {
+            DataTable dt = new DataTable();
+            db.ac();
+            SqlCommand komut = new SqlCommand("select * from Urun, S_Detay where Urun.Barkod_No=S_Detay.Urun_id and S_Detay.Sip_Kodu=@kod", db.baglanti);
+            komut.Parameters.AddWithValue("@kod", sipid);
+            SqlDataAdapter adp = new SqlDataAdapter(komut);
+            adp.Fill(dt);
+            db.kapat();
+            return dt;
+        }
+
+        public int sipariskontrol(int musid)
+        {
+            int cevap = 0;
+            db.ac();
+            SqlCommand komut = new SqlCommand("select count(*) from Siparis where Mus_id=@a and Satis_Durum=0", db.baglanti);
+            komut.Parameters.AddWithValue("@a", musid);
+            int sonuc = Convert.ToInt16(komut.ExecuteScalar());
+
+            if (sonuc == 1)
+            {
+                DataTable dt = new DataTable();
+                SqlCommand cmd = new SqlCommand("select * from Siparis where Mus_id=@b and Satis_Durum=0", db.baglanti);
+                komut.Parameters.AddWithValue("@b", musid);
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                adp.Fill(dt);
+                cevap = Convert.ToInt16(dt.Rows[0][0]);
+            }
+            db.kapat();
+            return cevap;
+        }
+
+        public bool sepetkontrol(int musid)
+        {
+            bool cevap = false;
+            db.ac();
+            SqlCommand komut = new SqlCommand("select count(*) Siparis where Mus_id=@a and Satis_Durum=0", db.baglanti);
+            komut.Parameters.AddWithValue("@a", musid);
+            int sonuc = Convert.ToInt16(komut.ExecuteScalar());
+
+            if (sonuc == 1)
+            {
+                cevap = true;
+            }
+            db.kapat();
+            return cevap;
+        }
+    }
+}
